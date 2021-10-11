a6441df5e36373dc823d0c2cf8075a38df4697a6
Michael-Blessing <michael.blessing005@gmail.com>
2021-10-11 14:48:58 +0200

    yeet

--- lib/flutter_flow/flutter_flow_widgets.dart
+++ lib/flutter_flow/flutter_flow_widgets.dart
@@ -1,7 +1,7 @@
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
-import '../Camera.dart';
+import '../Photo.dart';
 
 class FFButtonOptions {
   const FFButtonOptions({
