46c0aaecbbf4ee69649404b494bbd4176b258fbb
Noobosaurus <90990447+Noobosaurus@users.noreply.github.com>
2021-11-08 14:41:47 +0100

    Added Firebase?

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -6,6 +6,8 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:camera_web/camera_web.dart';
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:geolocator_web/geolocator_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
@@ -14,6 +16,8 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   CameraPlugin.registerWith(registrar);
+  FirebaseFirestoreWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
   GeolocatorPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
