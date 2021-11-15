af2aefff50223171884214037eee2af541b3c915
Noobosaurus <90990447+Noobosaurus@users.noreply.github.com>
2021-11-15 14:30:40 +0100

    UI

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -8,6 +8,8 @@
 import 'package:camera_web/camera_web.dart';
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:flutter_sound_web/flutter_sound_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:geolocator_web/geolocator_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
@@ -18,6 +20,8 @@ void registerPlugins(Registrar registrar) {
   CameraPlugin.registerWith(registrar);
   FirebaseFirestoreWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  FlutterSoundPlugin.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
   GeolocatorPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
