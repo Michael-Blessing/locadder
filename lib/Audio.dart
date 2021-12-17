5fca357e889ed2dd999acbd2393f6bf1bf87a918
Noobosaurus <90990447+Noobosaurus@users.noreply.github.com>
2021-12-17 08:19:45 +0100

    o

--- lib/Audio.dart
+++ lib/Audio.dart
@@ -1,4 +1,4 @@
-/*
+
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -170,4 +170,4 @@ class _AudioWidgetState extends State<AudioWidget> {
       ),
     );
   }
-}*/
+}
